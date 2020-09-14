ddf32d04b4b7d4de4b91b4dea653c77f897bcf9f
arun kumar <alekhya0801@gmail.com>
2020-09-14 11:42:21 +0530

    Added : remove all session attributes

--- src/main/java/com/bridgelabz/usermanagement/controller/Logout.java
+++ src/main/java/com/bridgelabz/usermanagement/controller/Logout.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Enumeration;
 
 @WebServlet("/Logout")
 public class Logout extends HttpServlet {
@@ -17,7 +18,11 @@ public class Logout extends HttpServlet {
         HttpSession session = request.getSession();
         User user = (User) session.getAttribute("user");
         new UserManagementService().setUserLogout(user.getUserId());
-        session.removeAttribute("user");
+        Enumeration<String> attributes = request.getSession().getAttributeNames();
+        while (attributes.hasMoreElements()) {
+            String attribute = attributes.nextElement();
+            session.removeAttribute(attribute);
+        }
         session.invalidate();
         request.getSession().setAttribute("message",new UserManagementService().convertToString(Messages.LOGOUT_SUCCESSFUL));
         response.sendRedirect("login");
